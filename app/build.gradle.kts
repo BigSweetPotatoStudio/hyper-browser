876b89074eb30e65709c786d2435958456582cfe
dadigua <0laopo0@gmail.com>
2026-06-23 22:29:56 +0800

    chore: prepare 0.1.8 beta release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.dadigua.hyperbrowser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "0.1.7"
+        versionCode = 15
+        versionName = "0.1.8-beta.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
